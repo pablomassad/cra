4cb44d616de5f330d19719909bee63afbde71cc7
Pablo Massad <pamassad@teco.com.ar>
2023-10-13 19:25:36 -0300

    Deploy Backoffice y Frontend con icons android

--- backoffice/src/environments.js
+++ backoffice/src/environments.js
@@ -1,6 +1,6 @@
 export const ENVIRONMENTS = {
     versionCode: 1,
-    versionName: '1.00.08',
+    versionName: '1.00.10',
     productName: 'crabo',
     packageName: 'com.pp.crabo',
     appName: 'crabo',
