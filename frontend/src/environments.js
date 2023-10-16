30f084cf7f7ba4c73ee5c64c50e1a03bce3ccea7
Pablo Massad <pamassad@teco.com.ar>
2023-10-16 09:22:24 -0300

    Ahora si anda FCM en movil

--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,6 +1,6 @@
 export const ENVIRONMENTS = {
     versionCode: 1,
-    versionName: '1.00.40',
+    versionName: '1.00.41',
     productName: 'CRA',
     packageName: 'com.pp.cra',
     appName: 'CRA',
--- frontend/src/firebase/messaging.js
+++ frontend/src/firebase/messaging.js
@@ -3,6 +3,9 @@ import { doc, setDoc } from 'firebase/firestore'
 import { getToken, onMessage, getMessaging } from 'firebase/messaging'
 import { main } from 'fwk-q-main'
 
+import { FCM } from '@capacitor-community/fcm'
+import { PushNotifications } from '@capacitor/push-notifications'
+
 export const FCM_TOKEN_COLLECTION = 'fcmTokens'
 export const FCM_TOKEN_KEY = 'fcmToken' // key for storing FCM token in Firestore
 const VAPID_KEY = 'BP6nPflTuZhSgdqiyDaPMLxYy3o2gvcMM_oUl1NFP-CkMIgnAiXfOKeOhrNbjhCUOKVNEosPR4U9j2t_NSLhjy4'
@@ -23,16 +26,11 @@ export async function saveMessagingDeviceToken (uid) {
     if (main.state.isMobile) {
         console.log('save msg mobile token')
         try {
-            const fcmToken = await getToken(getMessaging(), { vapidKey: VAPID_KEY })
+            const fcmToken = await getFCMToken()
             if (fcmToken) {
-                console.log('Got FCM mobile token:', fcmToken)
+                console.log('FCM  token:', fcmToken)
                 const tokenRef = doc(db, FCM_TOKEN_COLLECTION, uid)
                 await setDoc(tokenRef, { fcmToken })
-
-                onMessage(getMessaging(), (message) => {
-                    console.log('New foreground notification from Firebase Messaging!', message.notification)
-                    const c = new Notification(message.notification.title, { body: message.notification.body })
-                })
             } else {
                 requestNotificationsPermissions(uid)
             }
@@ -60,4 +58,43 @@ export async function saveMessagingDeviceToken (uid) {
             console.error('Unable to get messaging token.', error)
         }
     }
+}
+
+async function getFCMToken () {
+    const permission = await PushNotifications.requestPermissions()
+    return new Promise((resolve, reject) => {
+        if (permission.receive === 'granted') {
+            if (Capacitor.getPlatform() === 'ios') {
+                PushNotifications.register().then(() => {
+                    PushNotifications.addListener('registration', (token) => {
+                        FCM.getToken().then((result) => {
+                            console.log(result.token) // This is token for IOS
+                            resolve(result.token)
+                        }).catch((err) => {
+                            console.log('i am Error', err)
+                            resolve(null)
+                        })
+                    })
+                })
+            } else if (Capacitor.getPlatform() === 'android') {
+                console.log('@@@ Granted Android permissions')
+                try {
+                    PushNotifications.register().then(() => {
+                        PushNotifications.addListener('registration', ({ value }) => {
+                            resolve(value)
+                        })
+                        PushNotifications.addListener('registrationError', ({ err }) => {
+                            console.log('@@@ Token FAILED:', err)
+                            resolve(null)
+                        })
+                    })
+                } catch (error) {
+                    console.log('@@@ Error registration:', error)
+                }
+            }
+        } else {
+            alert('No Permission for Notifications!')
+            resolve(null)
+        }
+    })
 }
