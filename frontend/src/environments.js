fd15cf43e06395ce57c6e742cd584f0b123056f9
Pablo Massad <pamassad@teco.com.ar>
2023-11-27 13:51:39 -0300

    Fix firebase-messaging

--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,6 +1,6 @@
 export const ENVIRONMENTS = {
     versionCode: 1,
-    versionName: '1.01.14',
+    versionName: '1.01.15',
     productName: 'CRA',
     packageName: 'com.pp.cra',
     appName: 'CRA',
